a6bd3188e60f53a0cd9478f8bc09c8325cbf2896
zengxianyu <zengxianyu18@gmail.com>
2021-09-16 05:43:59 +0800

    test cript

---
+++ test.sh
@@ -0,0 +1,11 @@
+python test.py \
+	--batchSize 1 \
+	--nThreads 1 \
+	--name objrmv \
+	--dataset_mode testimage \
+	--image_dir ./datasets/places2sample1k_val/places2samples1k_crop256 \
+	--mask_dir ./datasets/places2sample1k_val/places2samples1k_256_mask_square128 \
+        --output_dir ./results \
+	--model inpaint \
+	--netG baseconv \
+        --which_epoch latest \
